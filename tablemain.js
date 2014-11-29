20f793bc83bb7d70df601d0f38d2e34f339416bb
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2014-11-28 18:03:24 -0600

    logged ship coords to firebase db

--- tablemain.js
+++ tablemain.js
@@ -1,4 +1,111 @@
+//fix up styling
+//maybe make a modal for when you first open up the page -- 'prepare for battle'
 //check if all the pieces are on the board and then display a begin button
+//clicking begin button should change firebase ready to true
+
+var gameRef;
+var myPlayerNumber;
+
+function go() {
+  var userId = prompt('Username?', 'Guest');
+  // Consider adding '/<unique id>' if you have multiple games.
+  gameRef = new Firebase(GAME_LOCATION);
+  assignPlayerNumberAndPlayGame(userId, gameRef);
+};
+
+// The maximum number of players.  If there are already
+// NUM_PLAYERS assigned, users won't be able to join the game.
+var NUM_PLAYERS = 2;
+
+// The root of your game data.
+var GAME_LOCATION = "https://adamsbattleship.firebaseio.com/";
+
+// A location under GAME_LOCATION that will store the list of
+// players who have joined the game (up to MAX_PLAYERS).
+var PLAYERS_LOCATION = 'player_list';
+
+// A location under GAME_LOCATION that you will use to store data
+// for each player (their game state, etc.)
+var PLAYER_DATA_LOCATION = 'player_data';
+
+
+// Called after player assignment completes.
+function playGame(myPlayerNumber, userId, justJoinedGame, gameRef) {
+  var playerDataRef = gameRef.child(PLAYER_DATA_LOCATION).child(myPlayerNumber);
+  alert('You are player number ' + myPlayerNumber +
+      '.  Your data will be located at ' + playerDataRef.toString());
+
+  if (justJoinedGame) {
+    alert('Doing first-time initialization of data.');
+    playerDataRef.set({userId: userId, state: 'game state'});
+  }
+}
+
+// Use transaction() to assign a player number, then call playGame().
+function assignPlayerNumberAndPlayGame(userId, gameRef) {
+  var playerListRef = gameRef.child(PLAYERS_LOCATION);
+  myPlayerNumber, alreadyInGame = false;
+
+  playerListRef.transaction(function(playerList) {
+    // Attempt to (re)join the given game. Notes:
+    //
+    // 1. Upon very first call, playerList will likely appear null (even if the
+    // list isn't empty), since Firebase runs the update function optimistically
+    // before it receives any data.
+    // 2. The list is assumed not to have any gaps (once a player joins, they
+    // don't leave).
+    // 3. Our update function sets some external variables but doesn't act on
+    // them until the completion callback, since the update function may be
+    // called multiple times with different data.
+    if (playerList === null) {
+      playerList = [];
+    }
+
+    for (var i = 0; i < playerList.length; i++) {
+      if (playerList[i] === userId) {
+        // Already seated so abort transaction to not unnecessarily update playerList.
+        alreadyInGame = true;
+        myPlayerNumber = i; // Tell completion callback which seat we have.
+        return;
+      }
+    }
+
+    if (i < NUM_PLAYERS) {
+      // Empty seat is available so grab it and attempt to commit modified playerList.
+      playerList[i] = userId;  // Reserve our seat.
+      myPlayerNumber = i; // Tell completion callback which seat we reserved.
+      return playerList;
+    }
+
+    // Abort transaction and tell completion callback we failed to join.
+    myPlayerNumber = null;
+  }, function (error, committed) {
+    // Transaction has completed.  Check if it succeeded or we were already in
+    // the game and so it was aborted.
+    if (committed || alreadyInGame) {
+      playGame(myPlayerNumber, userId, !alreadyInGame, gameRef);
+    } else {
+      alert('Game is full.  Can\'t join. :-(');
+    }
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 var carrier = [];
 var battleship = [];
@@ -7,38 +114,43 @@ var destroyer = [];
 var patrol = [];
 var unabated = true;
 
-var firebase = new Firebase("https://adamsbattleship.firebaseio.com/");
 
-firebase.set({
-  playerOneReady: false,
-  playerTwoReady: false,
-  playersTurn: 1,
-  attackedCell: 0,
-  hit: false,
-  playerOne: {
-    carrier: 5,
-    battleship: 4,
-    sub: 3,
-    destroyer: 3,
-    patrol: 2,
-    totalHealth: 17
-  },
-  playerTwo: {
-    carrier: 5,
-    battleship: 4,
-    sub: 3,
-    destroyer: 3,
-    patrol: 2,
-    totalHealth: 17
-  }
-});
+// firebase.set({
+//   playerOneReady: false,
+//   playerTwoReady: false,
+//   playersTurn: 1,
+//   attackedCell: 0,
+//   hit: false,
+//   playerOne: {
+//     carrier: 5,
+//     battleship: 4,
+//     sub: 3,
+//     destroyer: 3,
+//     patrol: 2,
+//     totalHealth: 17
+//   },
+//   playerTwo: {
+//     carrier: 5,
+//     battleship: 4,
+//     sub: 3,
+//     destroyer: 3,
+//     patrol: 2,
+//     totalHealth: 17
+//   }
+// });
 
 $(document).ready(function() {
 
+  go();
+
+  $( ".piece" ).draggable({ opacity: 0.6, revert: 'invalid', Index: 100, appenTo: 'td' });
+  $('.sea-1 td').droppable();
+
   // Mother function
-  $('.sea-1 td').click(function(){
+  $('.sea-1 td').on('drop', function(){
     var x = $(this).data("x");
     var y = $(this).data("y");
+    console.log(x +'' + y);
     var coordinates = [x, y];
     var length = shipLength(selectedPiece);
     var fitsOnSea = checkIfPieceFits(coordinates, length, orientation);
@@ -46,6 +158,7 @@ $(document).ready(function() {
     if (fitsOnSea && unabated) {
       renderPieces(coordinates, length, orientation, selectedPiece);
       hideFromFleet(selectedPiece);
+      checkIfAllPiecesArePlaced();
     } else {
       alert("Over the line!");
     }
@@ -120,7 +233,7 @@ $(document).ready(function() {
     }
   }
 
-  $("div.piece").click(function() {
+  $("div.piece").mousedown(function() {
     var pieceName = $(this).data("name");
     selectPiece(pieceName);
   });
@@ -147,21 +260,29 @@ $(document).ready(function() {
   }
 
   function logCoordinates(coordinates, selectedPiece) {
+    function fleetSet(boat) {
+      gameRef.child("player_data/" + myPlayerNumber + "/fleet/" + boat).push(coordinates);
+    }
     switch(selectedPiece) {
       case 'carrier':
         carrier.push(coordinates);
+        fleetSet('carrier');
         break;
       case 'battleship':
         battleship.push(coordinates);
+        fleetSet('battleship');
         break;
       case 'sub':
-        sub.push(coordinates)
+        sub.push(coordinates);
+        fleetSet('sub');
         break;
       case 'destroyer':
         destroyer.push(coordinates);
+        fleetSet('destroyer');
         break;
       case 'patrol':
         patrol.push(coordinates);
+        fleetSet('patrol');
     }
   }
 
@@ -209,14 +330,26 @@ $(document).ready(function() {
 
   function takeFire(coordinates) {
     var cellCoordinates = [coordinates[0], coordinates[1]];
-    // var cellElement = boardCell(cellCoordinates, 2);
-    firebase.set({
-      playerOneAttack: cellCoordinates
-    });
+    var cellElement = boardCell(cellCoordinates, 2);
   }
 
+  var allPiecesAreOnBoard = false;
+
+  function checkIfAllPiecesArePlaced() {
+    var occupiedSquares = $('.sea-1 .occupied').length;
+    if( occupiedSquares === 17) {
+      allPiecesAreOnBoard = true;
+      gameInit();
+    }
+  }
+
+  function gameInit() {
+    gameRef.child("player_data/" + myPlayerNumber + "/state").set('ready');
 
 
-
+    gameRef.child("player_data/" + myPlayerNumber + "/state").on("value", function(snapshot) {
+      alert(snapshot.val());
+    });
+  }
 
 });
